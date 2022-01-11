b6751b9f72a55c6ae22899e78da8fce33d9ab806
rachelvargas <25648752+rachelvargas@users.noreply.github.com>
2022-01-10 20:10:03 -0700

    Update main.js

--- Week01/JS/main.js
+++ Week01/JS/main.js
@@ -5,23 +5,23 @@ const links = [
   },
   {
     label: "Week02 notes",
-    url: "week02/index.html"
+    url: "Week02/index.html"
     },
   {
     label: "Week03 notes",
-    url: "week03/index.html"
+    url: "Week03/index.html"
   },
   {
     label: "Week04 notes",
-    url: "week04/index.html"
+    url: "Week04/index.html"
   },
   {
     label: "Week05 notes",
-    url: "week05/index.html"
+    url: "Week05/index.html"
   },
   {
     label: "Week06 notes",
-    url: "week06/index.html"
+    url: "Week06/index.html"
   },
   {
     label: "Week07 notes",
@@ -29,19 +29,19 @@ const links = [
   },
   {
     label: "Week08 notes",
-    url: "week08/index.html"
+    url: "Week08/index.html"
   },
   {
     label: "Week09 notes",
-    url: "week09/index.html"
+    url: "Week09/index.html"
   },
   {
     label: "Week10 notes",
-    url: "week10/index.html"
+    url: "Week10/index.html"
   },
   {
     label: "Week11 notes",
-    url: "week11/index.html"
+    url: "Week11/index.html"
   },
   {
     label: "Week12 notes",
@@ -54,5 +54,20 @@ const links = [
   
   
   
-  
-]
+];
+
+const ol = document.getElementById('assignments');      
+
+links.forEach(link => {
+
+  let li = document.createElement('li');
+
+  let a = document.createElement('a');
+
+  a.setAttribute('href', link.url);
+
+  a.innerText = link.label;
+
+  ol.appendChild(li);
+
+});
