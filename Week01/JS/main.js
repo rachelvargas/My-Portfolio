ceb2ed6a6325b29cd73d931acf668a40462387c2
rachelvargas <25648752+rachelvargas@users.noreply.github.com>
2022-01-12 18:08:25 -0700

    Update main.js

--- Week01/JS/main.js
+++ Week01/JS/main.js
@@ -6,53 +6,7 @@ const links = [
   {
     label: "Week02 notes",
     url: "Week02/index.html"
-    },
-  {
-    label: "Week03 notes",
-    url: "Week03/index.html"
-  },
-  {
-    label: "Week04 notes",
-    url: "Week04/index.html"
-  },
-  {
-    label: "Week05 notes",
-    url: "Week05/index.html"
-  },
-  {
-    label: "Week06 notes",
-    url: "Week06/index.html"
-  },
-  {
-    label: "Week07 notes",
-    url: "week07/index.html"
-  },
-  {
-    label: "Week08 notes",
-    url: "Week08/index.html"
-  },
-  {
-    label: "Week09 notes",
-    url: "Week09/index.html"
-  },
-  {
-    label: "Week10 notes",
-    url: "Week10/index.html"
-  },
-  {
-    label: "Week11 notes",
-    url: "Week11/index.html"
-  },
-  {
-    label: "Week12 notes",
-    url: "week12/index.html"
-  },
-  {
-    label: "Week13 notes",
-    url: "week13/index.html"
-  }
-  
-  
+    } 
   
 ];
 
