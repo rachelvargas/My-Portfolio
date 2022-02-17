95f1cbda2fdf586c1cc193eeadd0d12bfbd66e79
rachelvargas <25648752+rachelvargas@users.noreply.github.com>
2022-02-17 14:34:54 -0700

    Update utilities.js

--- Week06/utilities.js
+++ Week06/utilities.js
@@ -1,34 +1,34 @@
 //getting the text input content
-export function getContent(){
+export function getContent() {
     const content = document.getElementById('addTodo').value;
     return content;
 }
 
 //resetting the input content field to blank
-export function resetContent(){
+export function resetContent() {
     document.getElementById('addTodo').value = "";
 }
 
 //displaying the whole list
-export function renderList(parent, list){
+export function renderList(parent, list) {
     parent.innerHTML = "";
     list.forEach(toDo => {
         rendertoDo(toDo);
-        
+
     });
 }
 
 //displaying individual items from the list
-function rendertoDo(toDo){
+function rendertoDo(toDo) {
     const li = document.createElement('li');
     li.id = toDo.id;
-    if(toDo.completed){
+    if (toDo.completed) {
         //addint complete class name for later
         li.className = 'complete';
     }
     const text = document.createTextNode(toDo.content);
     li.appendChild(text);
-    document.createElement('toDoList').appendChild(li);
+    document.getElementById('toDoList').appendChild(li);
 
     //displaying close button
     const span = document.createElement('SPAN');
@@ -39,15 +39,14 @@ function rendertoDo(toDo){
     li.appendChild(span);
 }
 //sum the item that are left
-export function notCompleted(location, list){
-    if(list != null){
+export function notCompleted(location, list) {
+    if (list != null) {
         const count = list.length;
         location.innerText = 'Task left: ' + count;
 
     }
 
 }
-
 
     
 
