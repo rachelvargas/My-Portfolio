c70b064f24dd91a49f775343295c9e714316c93f
rachelvargas <25648752+rachelvargas@users.noreply.github.com>
2022-02-19 14:05:59 -0700

    Update main.js

--- Week06/main.js
+++ Week06/main.js
@@ -6,11 +6,14 @@ addButton.addEventListener('click', function() {
     const todo = new ToDo();
     todo.addingToList();
     todo.showList();
-    todo.addFilterEventListener();
+    // todo.addFilterEventListener();
+    //todo.addRemoveListener();
 });
 window.addEventListener('load', () => {
     //show a list of existing todos on load
     const todo = new ToDo();
+    todo.showList();
     todo.addCheckedListener();
-    todo.addFilterEventListener
+    todo.addFilterEventListener();
+    todo.addRemoveListener();
 });
