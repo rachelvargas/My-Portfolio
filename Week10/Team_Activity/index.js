b249d069fbb32754df64709a6adecbf7c8139e9f
rachelvargas <25648752+rachelvargas@users.noreply.github.com>
2022-03-11 13:42:26 -0700

    Update index.js

--- Week10/Team_Activity/index.js
+++ Week10/Team_Activity/index.js
@@ -1,5 +1,10 @@
+//STEP 04
+//starting the quakes module importing utilities.js and QuekesControllerjs
 import { getJSON, getLocation } from "./utilities.js";
 import QuakesController from "./QuakesConroller.js";
+
+//const url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
+
 
 const quake = new QuakesController('#quakeList');
 quake.init();
