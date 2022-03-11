58d083a90d122437da1dd54e03af7a7d7996dfd3
rachelvargas <25648752+rachelvargas@users.noreply.github.com>
2022-03-11 13:21:35 -0700

    Update index.js

--- Week10/Team_Activity/index.js
+++ Week10/Team_Activity/index.js
@@ -1,5 +1,5 @@
-import { getJSON, getLocation } from "./utilities";
-import QuakesController from "./QuakesConroller";
+import { getJSON, getLocation } from "./utilities.js";
+import QuakesController from "./QuakesConroller.js";
 
 const quake = new QuakesController('#quakeList');
 quake.init();
